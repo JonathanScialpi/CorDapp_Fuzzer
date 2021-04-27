c8ebd9f6a848be9f2c1b78d42fffeb62f83f92b4
Jonathan Scialpi <scialpi27@gmail.com>
2021-04-27 17:39:08 -0400

    using yargs to develop CLI

--- Fuzzer/collection_parser.js
+++ Fuzzer/collection_parser.js
@@ -1,8 +1,30 @@
-var fs = require('fs'),
-  Collection = require('postman-collection').Collection,
-  myCollection;
+var yargs = require('yargs');
 
-myCollection = new Collection(JSON.parse
-  (fs.readFileSync('Decentralized Corpus Manager.postman_collection.json').toString()));
+yargs.version('1.0.0');
 
+const argv = yargs
+.command('--path', 'Path to your Postman Collection',{
+  path: {
+    description: 'path to the postman collection file',
+    alias: 'p',
+    type: 'string',
+  }
+})
+.option('runs',{
+  alias: 'r',
+  description: 'number of test runs per discovered endpoint',
+  type: 'int',
+})
+.help()
+.showHelpOnFail(true, "Specify --help for available options")
+.alias('help','h')
+.argv;
+
+if (!argv.path){
+  throw new Error("The path variable is required");
+}
+
+var fs = require('fs'),  Collection = require('postman-collection').Collection,  myCollection;
+
+myCollection = new Collection(JSON.parse  (fs.readFileSync(argv.path).toString()));
 console.log(myCollection.toJSON());

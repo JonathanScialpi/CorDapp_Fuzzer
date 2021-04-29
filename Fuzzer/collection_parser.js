79478bb4bde320ec8e15b2fcb5efc5f6d8a48727
Jonathan Scialpi <scialpi27@gmail.com>
2021-04-29 11:11:24 -0400

    added structure to support different post body modes

--- Fuzzer/collection_parser.js
+++ Fuzzer/collection_parser.js
@@ -33,23 +33,23 @@ myCollection = new Collection(JSON.parse  (fs.readFileSync(argv.path).toString()
 myCollection.forEachItem(
   
   function(item){
-    switch(item.request.method) {
-      case "POST":
-        execURLEncodedRequest(
-          item.request.url.getRaw(), 
-          buildDataMap(item.request.body.urlencoded.members)
-          );
-        break;
-      default:
-          execBasicGET(item.request.url.getRaw());
-    };
+    if(item.request.method == "POST"){
+      postRequestByMode(item);
+    }
+    else if(item.request.method == "GET"){
+      dynamicRequest(item.request.method, item.request.url.getRaw(), buildDataMap());
+    }
+    else{
+      throw new Error(item.request.method + " is not currently supported.");
+    }
   }
 );
 
-function execBasicGET(url){
+function dynamicRequest(method, url, dataMap){
   axios({
-    method: "get",
-    url: url
+    method: method.toLowerCase(),
+    url: url,
+    data: dataMap
   })
   .then(function (response) {
     console.log(response);
@@ -62,27 +62,25 @@ function execBasicGET(url){
   });
 }
 
-function execURLEncodedRequest(url, data){
-  axios({
-    method: "post",
-    url: url,
-    data:  querystring.stringify(data)
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-}
-function buildDataMap(urlencodedMembers){
-  var dataMap = {};
+function buildDataMap(params=[]){
+  var dataMap = {}
   var index;
-  for(index in urlencodedMembers){
-    dataMap[urlencodedMembers[index].key] = urlencodedMembers[index].value;
+  for(index in params){
+    dataMap[params[index].key] = params[index].value;
   }
   return dataMap;
+}
+
+function postRequestByMode(item){
+  switch(item.request.body.mode) {
+    case "FillerMode1":
+      // code block
+      break;
+    case "FillerMode2":
+      // code block
+      break;
+    default:
+      // default POST request will be executed as urlencoded for now
+      dynamicRequest(item.request.method, item.request.url.getRaw(), querystring.stringify(buildDataMap(item.request.body.urlencoded.members)));
+  };
 }

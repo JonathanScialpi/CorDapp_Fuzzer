f4e77c9ebdbf1dbe2b4c98e3aca4d7d1bc0dc47f
Jonathan Scialpi <scialpi27@gmail.com>
2021-05-03 17:39:26 -0400

    fuzzing with a provided config - numbers, text variants, and x500s

--- Fuzzer/collection_parser.js
+++ Fuzzer/collection_parser.js
@@ -1,49 +1,62 @@
 const axios = require('axios');
 const querystring = require('querystring');
 var yargs = require('yargs');
+var shuffle = require('shuffle-array');
 
 yargs.version('1.0.0');
 
 const argv = yargs
-.command('--path', 'Path to your Postman Collection',{
-  path: {
+.command('--pmanCollection', 'Path to your Postman Collection',{
+  pmanCollection: {
     description: 'path to the postman collection file',
-    alias: 'p',
+    alias: 'pman',
     type: 'string',
   }
 })
-.option('runs',{
-  alias: 'r',
-  description: 'number of test runs per discovered endpoint',
-  type: 'int',
+.command('--paramConfig', 'Path to your Postman Collection',{
+  paramConfig: {
+    description: 'path to the postman collection file',
+    alias: 'conf',
+    type: 'string',
+  }
 })
 .help()
 .showHelpOnFail(true, "Specify --help for available options")
 .alias('help','h')
 .argv;
 
-if (!argv.path){
-  throw new Error("The path variable is required");
+//-------------------------------------------------- main start --------------------------------------------------
+
+if (!argv.pmanCollection){
+  throw new Error("The postman collection command is required");
+}else if(!argv.paramConfig){
+  throw new Error ("The config command is required");
 }
 
 var fs = require('fs'),  Collection = require('postman-collection').Collection,  myCollection;
 
-myCollection = new Collection(JSON.parse  (fs.readFileSync(argv.path).toString()));
+myCollection = new Collection(JSON.parse(fs.readFileSync(argv.pmanCollection).toString()));
+let config = JSON.parse(fs.readFileSync(argv.paramConfig));
+// console.log(type(config))
 
+// First run of all requests to test that they work and to attain the peerlist
 myCollection.forEachItem(
-  
+
   function(item){
     if(item.request.method == "POST"){
-      postRequestByMode(item);
+      parseConfig(item);
     }
     else if(item.request.method == "GET"){
-      dynamicRequest(item.request.method, item.request.url.getRaw(), buildDataMap());
+      // dynamicRequest(item.request.method, item.request.url.getRaw(), buildDataMap());
+      console.log("Skipping GETs for now...")
     }
     else{
       throw new Error(item.request.method + " is not currently supported.");
     }
   }
 );
+
+//-------------------------------------------------- main end --------------------------------------------------
 
 function dynamicRequest(method, url, dataMap){
   axios({
@@ -57,21 +70,58 @@ function dynamicRequest(method, url, dataMap){
   .catch(function (error) {
     console.log(error);
   })
-  .then(function () {
-    // always executed
-  });
+  // .then(function () {})
+  ;
 }
 
-function buildDataMap(params=[]){
+function parseConfig(_item){
+  var params = _item.request.body.urlencoded.members;
+  //search all parameters in current request   
+  for(var index in params){
+     
+      var currentKey = params[index].key;
+       // if we find one that is also in the config then we can start generating tests
+      if(currentKey in config){
+        for(fuzzerIndex in config[currentKey]["fuzzers"]){
+          for(var i = 0; i < config[currentKey]["runsPerFuzz"]; i++){
+            postRequestByMode(
+              _item,
+              buildFuzzedRequest(
+                currentKey,
+                config[currentKey]["fuzzers"][fuzzerIndex], 
+                params
+              )
+            );
+          }
+        }
+      }
+    }
+}
+
+function buildFuzzedRequest(_keyTofuzz, _fuzzerType, _params=[]){
   var dataMap = {}
-  var index;
-  for(index in params){
-    dataMap[params[index].key] = params[index].value;
+     for(var index in _params){
+      dataMap[_params[index].key] = _params[index].value;
+    }
+  
+  if(_fuzzerType == "numbers"){
+    dataMap[_keyTofuzz] = numberFuzzer();
+  }else{
+    dataMap[_keyTofuzz] = textFuzzer(_fuzzerType);
+  }
+  
+  if(dataMap[config["peerParam"]]){
+    dataMap[config["peerParam"]] = getRandomPeer(
+      [...config[config["peerParam"]]["peerList"]], 
+      config[config["peerParam"]]["minPeers"],
+      config[config["peerParam"]]["maxPeers"]
+      );
+      console.log("PEER WAS: " + dataMap["partyName"])
   }
   return dataMap;
 }
 
-function postRequestByMode(item){
+function postRequestByMode(item, requestData){
   switch(item.request.body.mode) {
     case "FillerMode1":
       // code block
@@ -81,6 +131,85 @@ function postRequestByMode(item){
       break;
     default:
       // default POST request will be executed as urlencoded for now
-      dynamicRequest(item.request.method, item.request.url.getRaw(), querystring.stringify(buildDataMap(item.request.body.urlencoded.members)));
+
+      // first make a request with existing values in collection to 
+      dynamicRequest(
+        item.request.method, 
+        item.request.url.getRaw(), 
+        querystring.stringify(
+          // buildDataMap(item.request.body.urlencoded.members)
+          requestData
+          ));
   };
+}
+
+/* 
+Fuzzer options: number, text, x500name, x500nameFromList
+Params to apply to: Default is all, listOfKeys to apply to
+*/
+
+// random number of decimal points between 0-3 decimals
+// random number between int min and int max
+function numberFuzzer(min=Number.MIN_SAFE_INTEGER, max=Number.MAX_SAFE_INTEGER, decimalsMin=1, decimalsMax=4){
+  var decimal = 0;
+  if(decimalsMax > 1){
+      var decimals = Math.floor(Math.random() * (decimalsMax - decimalsMin) + decimalsMin);
+  // console.log("number of decimals: " + decimals)
+
+      var decimalMax = 10**decimals
+      // console.log("decimalMax: " + decimalMax)
+
+      var decimalNumber = Math.floor(Math.random() * decimalMax);
+      // console.log("decimalNumber: " + decimalNumber)
+  
+      decimal = decimalNumber/decimalMax
+      // console.log("decimal: " + decimal)
+  }
+
+  var mainNum = Math.random() * (max - min) + min;
+  // console.log("main numb: " + mainNum)
+
+  return Math.floor(mainNum) + decimal;
+}
+/*
+Min / Max length
+Random a-z/A-Z: AJnvaKLJzsd
+Random a-z/A-Z extended languages: 
+Random a-z/A-Z + numbers: s8Av9apJd1
+Random a-z/A-Z + symbols: aJ
+*/
+function textFuzzer(fuzzType="text", minLength=1, maxLength=100){
+    
+  var characterLib = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+  //choose library
+  switch(fuzzType) {
+      case "textExtendedLang":
+          characterLib = characterLib.concat(require('@unicode/unicode-11.0.0/General_Category/Lowercase_Letter/symbols.js')).concat(require('@unicode/unicode-11.0.0/General_Category/Lowercase_Letter/symbols.js'));
+          break;
+      case "textAndNumbers":
+          characterLib = characterLib.concat(['0','1','2','3','4','5','6','7','8','9']);
+          break;
+      case "textAndSymbols":
+          characterLib = characterLib.concat(require('@unicode/unicode-11.0.0/General_Category/Symbol/symbols.js')); 
+          break;
+      default:
+          characterLib = characterLib
+  }
+  var textLength = numberFuzzer(minLength, maxLength, 0, 0)
+  var fuzzedText = []
+  while(fuzzedText.length < textLength){
+      fuzzedText.push(characterLib[numberFuzzer(0, characterLib.length, 0, 0)]);
+  }
+  return fuzzedText.join('');
+}
+
+function getRandomPeer(_peerList, _minPeers=1, _maxPeers=2){
+  shuffle(_peerList)
+  var peerListSize = numberFuzzer(_minPeers, _maxPeers, 0, 0)
+  var peers = []
+  while(peers.length < peerListSize){
+    peers.push(_peerList.pop())
+  }
+  return peers; 
 }
